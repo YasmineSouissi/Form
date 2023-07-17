96c7d1e60c20bc01b322c1a0806632b031af7f5c
Yasmine Souissi <souissiy35@gmail.com>
2023-07-17 15:56:42 +0100

    added input validation in the sign up page

--- Form/src/app/services/user-services.service.ts
+++ Form/src/app/services/user-services.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../Model/user';
+import * as crypto from 'crypto';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class UserServicesService {
   url=' http://localhost:3000/users';
   users: User[]= [];
 
-  constructor(private http: HttpClient) {}
+  constructor(public http: HttpClient) {}
    getUsers(){
       return this.http.get<User[]>(this.url);
    }
@@ -18,20 +19,12 @@ export class UserServicesService {
   }
   
   findUserIndexByEmail(email: string): number {
-    this.getUsers().subscribe((data: User[]) => {
-      this.users = data;
-      //console.log("USERS:", this.users); 
-    });
     const index = this.users.findIndex(user => user.email === email);
-    //console.log(this.users);
-    //console.log("findUserIndexByEmail ",index);
     return index;
-    
   }
   checkUsersEmail(user: User): boolean {
-   
-    let index = this.findUserIndexByEmail(user.email);
-    //console.log("checkUsersEmail",index!=-1);
-    return (index!=-1);
+    const index = this.findUserIndexByEmail(user.email);
+    return (index !== -1);
   }
+ 
   }
--- Form/src/app/sign-up/sign-up.component.ts
+++ Form/src/app/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { User } from '../Model/user';
 import { NgForm } from '@angular/forms';
 import { UserServicesService } from '../services/user-services.service';
+import { Observable, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
@@ -12,30 +13,72 @@ export class SignUpComponent {
 
   users: User[]= [];
   user: User=new User();
+  existingEmail:boolean=false;
   constructor(public userServ: UserServicesService){}
   ngOnInit():void {
     this.userServ.getUsers().subscribe(
       (data: User[]) => this.users=data);
   }
 
-  submit(form: NgForm) {
-    User.id++; 
-    this.userServ.addUser(this.user).subscribe(
-        () => {
-        this.userServ.getUsers().subscribe((data: User[]) => {
-          this.users = data;
-          form.reset(); 
-          this.user = new User(); 
-        });
-      },
-      (error) => {
-      
-        console.error('Error adding user:', error);
-      }
+  submit(): Observable<User | User[]> {
+    return this.userServ.http.get<User[]>(this.userServ.url).pipe(
+      switchMap((users: User[]) => {
+        const existingUser = users.find(u => u.email === this.user.email);
+        this.existingEmail=(existingUser!=undefined);
+        //console.log("existing user:",existingUser);
+        if (existingUser) {
+        //console.log("existing user:",existingUser);
+          return of(existingUser);
+        } else {
+        //console.log("existing user:",existingUser);
+          return this.userServ.addUser(this.user);
+        }
+      })
     );
   }
-  
-  
-  
-  
+
+onSubmit() {
+  this.submit().subscribe(
+    (response: User | User[]) => {
+      if (Array.isArray(response)) {
+        console.log('New user added successfully');
+       
+      } else {
+        console.log('Existing user:', response);
+       
+      }
+    },
+    (error: any) => {
+      console.error('Error checking and adding user:', error);
+      
+    }
+  );
 }
+
+
+  /*submit(form: NgForm) {
+    User.id++;
+    if (!this.userServ.checkUsersEmail(this.user)) {
+      this.userServ.addUser(this.user).subscribe(
+        () => {
+          this.userServ.getUsers().subscribe((data: User[]) => {
+            this.users = data;
+            form.reset();
+            this.user = new User();
+            console.log("user added successfully");
+          });
+        },
+        (error) => {
+          console.error('Error adding user:', error);
+        }
+      );
+    } else {
+      this.userServ.getUsers().subscribe((data: User[]) => {
+        this.users = data;
+        this.existingEmail = true;
+        console.log("user already exists");
+      });
+  }*/
+}
+
+
