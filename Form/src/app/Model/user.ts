3f59b6a4d7e72b9de7e6020b63e82f54536c92bd
Yasmine Souissi <souissiy35@gmail.com>
2023-07-19 14:35:44 +0100

    added phone number field

--- Form/src/app/Model/user.ts
+++ Form/src/app/Model/user.ts
@@ -1,7 +1,10 @@
-export class User{
-    static id: number=0;
-    email: string="";
-    password: string="";
+export class User {
+    id: number = 0;
+    email: string = '';
+    password: string = '';
+    phoneNumber: number | undefined; 
+  
+  
     constructor(){
 
     }
